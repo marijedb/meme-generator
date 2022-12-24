9eefeaad673badcf5dcfc7bb1923240987117612
marijedb <marijedb@live.nl>
2022-12-24 21:12:07 +0100

    inital personal setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+        <div className="navbar">
+            <h3 className="navbar--title">Meme Generator</h3>
+            <p className="navbar--p">Made by <a className="navbar--link" href="https://github.com/marijedb" target="blank">Marije</a></p>
+        </div>
+    )
+}
+
+export default Navbar;
