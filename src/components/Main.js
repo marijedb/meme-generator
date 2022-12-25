922645f10143b2ea15f6e30d375333152f230df8
marijedb <marijedb@live.nl>
2022-12-25 15:56:44 +0100

    main component updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,68 @@
-function Main() {
-    return(
-        <div>
+import {useState, useEffect} from "react"
 
+function Main() {
+
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        memeImage: ""
+    }) 
+        
+    const [memeImages, setMemeImages] = useState({})
+
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
+    useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes')
+            .then(response => response.json())
+            .then(memesData => setMemeImages(memesData.data.memes))
+    },[])
+
+    function getMemeImage(){
+        const randomNumber = Math.floor(Math.random() * memeImages.length)
+        const url = memeImages[randomNumber].url
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                memeImage: url
+            }
+        })
+    }
+
+
+    return(
+        <div className="main">
+            <div className="form">
+                <input 
+                    className="form--input-top"
+                    type="text"
+                    name="topText"
+                    placeholder="Top text here" 
+                    value={meme.topText}  
+                    onChange={handleChange}
+                />
+                <input 
+                    className="form--input-bottom"
+                    type="text"
+                    name="bottomText"
+                    placeholder="Bottom text here" 
+                    value={meme.bottomText}  
+                    onChange={handleChange}
+                />
+                <button className="form--button" onClick={getMemeImage}>
+                    Get a new Meme image
+                </button>
+            </div>
+            {meme.memeImage && <img className="main--image" src={meme.memeImage} alt="meme"/>}
         </div>
     )
 }
